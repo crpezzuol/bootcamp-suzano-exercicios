d52f0fb8c8bdaca2ff0f31a84fefcd8b740d1287
CARLOS PEZZUOL <165528245+crpezzuol@users.noreply.github.com>
2025-05-17 23:09:22 -0300

    Create 00_estruturas_listas

---
+++ 00_estruturas_listas.py
@@ -0,0 +1,27 @@
+frutas = ['laranja', 'maça', 'uva', 'banana', 'pera']
+print(frutas)
+
+frutas[0] = 'kiwi'
+print(frutas) 
+
+frutas[1] = 'manga'
+print(frutas)
+
+frutas[2:3] = 'abacaxi', 'morango'
+print(frutas)
+
+frutas = []
+print (frutas)
+
+letras = list('python')
+print(letras)
+
+nome = 'Carlos'
+nomes = list(nome)
+print(nomes)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ['Ferrari', 'F8', 420000, 2020, 2900, 'São Paulo', True]
+print(carro)
